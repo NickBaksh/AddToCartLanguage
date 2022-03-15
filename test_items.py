7a2f84d424f3ee6692786cf6dfad0f740f4e7ec8
NickBaksh <46294747+NickBaksh@users.noreply.github.com>
2022-03-15 23:15:48 +0300

    imports updated

--- test_items.py
+++ test_items.py
@@ -1,10 +1,15 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_language_shopping_cart(browser):
     browser.get(link)
     button = browser.find_element_by_css_selector(".btn-add-to-basket")
-
+    WebDriverWait(browser, 5).until(EC.visibility_of_element_located
+                                    ((By.CSS_SELECTOR, ".btn-add-to-basket")), "No such button: 'Add to cart'")
     assert button is not None, f"Button 'Add to cart' is not available"
 
 
